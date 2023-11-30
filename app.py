5c33b11954ccb82842eac7bab0f6f9777b59a6fb
Laisky.Cai <github@laisky.com>
2023-11-30 02:21:49 +0000

    feat: Refactor code for improved performance
    - Add and organize imports relevant to the new functionality
    - Refactor initialization of objects to improve performance and handle different scenarios
    - Update route and port to enhance application scalability and security

--- app.py
+++ app.py
@@ -19,6 +19,7 @@ import base64
 import io
 import torch
 import aiohttp.web
+import random
 
 if torch.cuda.is_available():
     device = "cuda"
@@ -30,11 +31,17 @@ else:
 
 pipes = {
     "txt2img": AutoPipelineForText2Image.from_pretrained(
-        "stabilityai/sdxl-turbo", torch_dtype=torch.float16, variant="fp16"
-    ).to(device),
+        "stabilityai/sdxl-turbo",
+        torch_dtype=torch.float16,
+        variant="fp16",
+        requires_safety_checker=False,
+    ).to("cuda"),
     "img2img": AutoPipelineForImage2Image.from_pretrained(
-        "stabilityai/sdxl-turbo", torch_dtype=torch.float16, variant="fp16"
-    ).to(device),
+        "stabilityai/sdxl-turbo",
+        torch_dtype=torch.float16,
+        variant="fp16",
+        requires_safety_checker=False,
+    ).to("cuda"),
 }
 
 
@@ -43,22 +50,48 @@ async def handler(request: aiohttp.web.Request) -> aiohttp.web.Response:
     assert data["text"], "text is required"
     prompt = data["text"]
 
-    resp: aiohttp.web.Response
-    if data['image']:
-        base64_image: str = data["image"]
-        image_bytes = base64.b64decode(base64_image)
-        image = load_image(Image.open(io.BytesIO(image_bytes)))
-        image = pipes["img2img"](prompt, image)[0]
-        resp = aiohttp.web.Response(body=image, content_type="image/png")
-    else:
-        image = pipes["txt2img"](prompt)
-        resp = aiohttp.web.Response(body=image, content_type="image/png")
+    generator = torch.Generator("cuda").manual_seed(random.randint(0, 1000000))
 
+    resp: aiohttp.web.Response
+    if data.get("image"):
+        base64_image: str = data["image"]
+        base64_image = base64_image.removeprefix("data:image/png;base64,")
+        image_bytes = base64.b64decode(base64_image)
+        src_image = load_image(Image.open(io.BytesIO(image_bytes)))
+
+        # draw image
+        result_pil_image = pipes["img2img"](
+            prompt=prompt,
+            image=src_image,
+            generator=generator,
+            height=512,
+            width=512,
+            guidance_scale=0.5,
+            num_inference_steps=4,
+        ).images[0]
+
+        # convert to png
+        byte_arr = io.BytesIO()
+        result_pil_image.save(byte_arr, format="PNG")
+        resp = aiohttp.web.Response(body=byte_arr.getvalue(), content_type="image/png")
+    else:
+        result_pil_image = pipes["txt2img"](
+            prompt=prompt,
+            generator=generator,
+            height=512,
+            width=512,
+            guidance_scale=0.5,
+            num_inference_steps=4,
+        ).images[0]
+        # convert to png
+        byte_arr = io.BytesIO()
+        result_pil_image.save(byte_arr, format="PNG")
+        resp = aiohttp.web.Response(body=byte_arr.getvalue(), content_type="image/png")
 
     return resp
 
 
 if __name__ == "__main__":
     app = aiohttp.web.Application()
-    app.add_routes([aiohttp.web.post("/", handler)])
-    aiohttp.web.run_app(app, host="0.0.0.0", port=7860)
+    app.add_routes([aiohttp.web.post("/predict", handler)])
+    aiohttp.web.run_app(app, host="0.0.0.0", port=7861)
