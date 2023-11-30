e3875fe82b25a071034c9e94511d7f3929ae78c4
Laisky.Cai <github@laisky.com>
2023-11-30 01:34:52 +0000

    chore: Refactor environment variables and response handling in app.py
    - Update app.py with proxy environment variables for HTTP and HTTPS
    - Refactor response handling for different pipe types in app.py

--- app.py
+++ app.py
@@ -1,3 +1,17 @@
+import os
+
+os.environ["HTTP_PROXY"] = "http://100.122.41.16:17777"
+os.environ["HTTPS_PROXY"] = "http://100.122.41.16:17777"
+os.environ[
+    "NO_PROXY"
+] = "localhost,127.0.0.1,100.64.0.0/10,192.168.0.0/16,10.0.0.0/8,127.0.0.0/8,snake-carp.ts.net"
+os.environ["http_proxy"] = "http://100.122.41.16:17777"
+os.environ["https_proxy"] = "http://100.122.41.16:17777"
+os.environ[
+    "no_proxy"
+] = "localhost,127.0.0.1,100.64.0.0/10,192.168.0.0/16,10.0.0.0/8,127.0.0.0/8,snake-carp.ts.net"
+
+
 from diffusers import AutoPipelineForText2Image, AutoPipelineForImage2Image
 from diffusers.utils import load_image
 import torch
@@ -24,17 +38,22 @@ pipes = {
 async def handler(request: aiohttp.web.Request) -> aiohttp.web.Response:
     data = await request.json()
     pipe = pipes[data["type"]]
+    resp: aiohttp.web.Response
     if data["pipe"] == "txt2img":
         text = data["text"]
         image = pipe(text)
-        return aiohttp.web.Response(body=image, content_type="image/png")
+        resp = aiohttp.web.Response(body=image, content_type="image/png")
     elif data["pipe"] == "img2img":
         image = load_image(data["image"])
         image = pipe(image)
-        return aiohttp.web.Response(body=image, content_type="image/png")
+        resp = aiohttp.web.Response(body=image, content_type="image/png")
+    else:
+        resp = aiohttp.web.HTTPBadRequest(reason="Invalid pipe")
+
+    return resp
 
 
 if __name__ == "__main__":
     app = aiohttp.web.Application()
     app.add_routes([aiohttp.web.post("/", handler)])
-    aiohttp.web.run_app(app, host="0.0.0.0", port=8000)
+    aiohttp.web.run_app(app, host="0.0.0.0", port=7860)
