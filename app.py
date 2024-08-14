78d25541dc5dabd675c671762651955f6fae4401
Laisky.Cai <github@laisky.com>
2024-08-14 01:51:40 +0000

    refactor: Update DiffusionPipeline imports and add login step

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ import aiohttp.web
 import torch
 from PIL import Image
 
-from models import sdxlturbo
+# from models import sdxlturbo
 from models.gemma import predict as gemma_completions
 
 DEVICE: str
@@ -33,72 +33,72 @@ else:
     DEVICE = "cpu"
 
 
-async def handler(request: aiohttp.web.Request) -> aiohttp.web.Response:
-    """draw image with prompt or image by sdxl-turbo
+# async def handler(request: aiohttp.web.Request) -> aiohttp.web.Response:
+#     """draw image with prompt or image by sdxl-turbo
 
-    HTTP POST:
-    ::
-        {
-            "n": 1,
-            "model": "sdxl-turbo",
-            "text": "panda"
-            "image": "data:image/png;base64,xxxxx"
-        }
+#     HTTP POST:
+#     ::
+#         {
+#             "n": 1,
+#             "model": "sdxl-turbo",
+#             "text": "panda"
+#             "image": "data:image/png;base64,xxxxx"
+#         }
 
-    Returns:
-        image bytes in png format
-    """
-    data = await request.json()
+#     Returns:
+#         image bytes in png format
+#     """
+#     data = await request.json()
 
-    prompt = data.get("text")
-    n_images = data.get("n", 1)
+#     prompt = data.get("text")
+#     n_images = data.get("n", 1)
 
-    images: List[Image.Image] = []
-    videos: List[bytes] = []
-    model = data.get("model", "sdxl-turbo")
-    if model == "sdxl-turbo":
-        if data.get("image"):
-            images = sdxlturbo.img2img(
-                prompt=prompt,
-                negative_prompt=data.get("negative_prompt"),
-                b64img=data["image"],
-                n_images=n_images,
-            )
-        else:
-            images = sdxlturbo.txt2img(
-                prompt=prompt,
-                negative_prompt=data.get("negative_prompt"),
-                n_images=n_images,
-            )
-    elif model == "svd-xt":
-        video = sdxlturbo.img2video(
-            b64img=data["image"],
-        )
-        videos = [video]
-    else:
-        raise ValueError(f"there is no model named {model}")
+#     images: List[Image.Image] = []
+#     videos: List[bytes] = []
+#     model = data.get("model", "sdxl-turbo")
+#     if model == "sdxl-turbo":
+#         if data.get("image"):
+#             images = sdxlturbo.img2img(
+#                 prompt=prompt,
+#                 negative_prompt=data.get("negative_prompt"),
+#                 b64img=data["image"],
+#                 n_images=n_images,
+#             )
+#         else:
+#             images = sdxlturbo.txt2img(
+#                 prompt=prompt,
+#                 negative_prompt=data.get("negative_prompt"),
+#                 n_images=n_images,
+#             )
+#     elif model == "svd-xt":
+#         video = sdxlturbo.img2video(
+#             b64img=data["image"],
+#         )
+#         videos = [video]
+#     else:
+#         raise ValueError(f"there is no model named {model}")
 
-    response: Dict[str, List] = {}
+#     response: Dict[str, List] = {}
 
-    # convert to png
-    for img in images:
-        response.setdefault("images", [])
-        byte_arr = io.BytesIO()
-        img.save(byte_arr, format="PNG")
-        response["images"].append(
-            f"data:image/png;base64,{base64.b64encode(byte_arr.getvalue()).decode()}"
-        )
+#     # convert to png
+#     for img in images:
+#         response.setdefault("images", [])
+#         byte_arr = io.BytesIO()
+#         img.save(byte_arr, format="PNG")
+#         response["images"].append(
+#             f"data:image/png;base64,{base64.b64encode(byte_arr.getvalue()).decode()}"
+#         )
 
-    for video in videos:
-        response.setdefault("videos", [])
-        response["videos"].append(
-            f"data:video/mp4;base64,{base64.b64encode(video).decode()}",
-        )
+#     for video in videos:
+#         response.setdefault("videos", [])
+#         response["videos"].append(
+#             f"data:video/mp4;base64,{base64.b64encode(video).decode()}",
+#         )
 
-    resp = aiohttp.web.Response(
-        body=json.dumps(response), content_type="application/json"
-    )
-    return resp
+#     resp = aiohttp.web.Response(
+#         body=json.dumps(response), content_type="application/json"
+#     )
+#     return resp
 
 
 async def text_predict(request: aiohttp.web.Request) -> aiohttp.web.Response:
@@ -121,7 +121,7 @@ async def text_predict(request: aiohttp.web.Request) -> aiohttp.web.Response:
 if __name__ == "__main__":
     app = aiohttp.web.Application(client_max_size=1024**2 * 30)
 
-    app.add_routes([aiohttp.web.post("/predict", handler)])
+    # app.add_routes([aiohttp.web.post("/predict", handler)])
     app.add_routes([aiohttp.web.post("/chat/completions", text_predict)])
 
     aiohttp.web.run_app(app, host="0.0.0.0", port=7861)
